5406e220e4038d9daedbf834ea9329bdbf1ac77a
PORKWOTONLEE <675668376@qq.com>
2025-03-01 00:32:25 +0800

    add bsp rcc

--- Makefile
+++ Makefile
@@ -64,7 +64,8 @@ spl_lib/Drivers/CMSIS/CM3/CoreSupport/core_cm3.c \
 spl_lib/Core/Src/system_stm32f10x.c \
 userapp/src/userapp.c \
 bsp/src/bsp_led.c \
-bsp/src/bsp_key.c
+bsp/src/bsp_key.c \
+bsp/src/bsp_rcc.c
 
 # ASM sources
 ASM_SOURCES =  \
--- userapp/src/userapp.c
+++ userapp/src/userapp.c
@@ -1,24 +1,18 @@
 #include "userapp.h"
 #include "bsp_led.h"
-#include "bsp_key.h"
+#include "bsp_rcc.h"
 
 int userapp(void)
 {
+    HSE_SetSysCLock(RCC_PLLMul_9);
     LED_GPIO_Init();
-    Key_GPIO_Init();
 
     while (1)
     {
-        if (Key_Scan(KEY1_GPIO_PORT, KEY1_GPIO_PIN))
-        {
-            LED1_TOGGLE;
-            LED2_TOGGLE;
-            LED3_TOGGLE;
-        }
-        else
-        {
-
-        }
+        LED1_TOGGLE;
+        LED2_TOGGLE;
+        LED3_TOGGLE;
+        Delay(1000000);
     }
 
     return 0;
